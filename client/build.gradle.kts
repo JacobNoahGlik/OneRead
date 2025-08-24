30189d048f6a3c300be7312d3221ef489f95e26e
Jacob Glik <70964953+JacobNoahGlik@users.noreply.github.com>
2025-08-24 18:58:00 -0400

    Create build.gradle.kts

---
+++ client/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+kotlin("jvm") version (rootProject.extra["kotlinVersion"] as String)
+application
+}
+
+
+application { mainClass.set("com.oneread.ClientKt") }
+
+
+dependencies {
+val ktor = "2.3.12"
+implementation("io.ktor:ktor-client-core-jvm:$ktor")
+implementation("io.ktor:ktor-client-cio-jvm:$ktor")
+implementation("io.ktor:ktor-client-content-negotiation-jvm:$ktor")
+implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor")
+implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+}
