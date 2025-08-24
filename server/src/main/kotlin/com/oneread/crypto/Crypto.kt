087f99a6c0dd3116204d42b08fd7ed5541576681
Jacob Glik <70964953+JacobNoahGlik@users.noreply.github.com>
2025-08-24 18:54:58 -0400

    Create Crypto.kt

---
+++ server/src/main/kotlin/com/oneread/crypto/Crypto.kt
@@ -0,0 +1,27 @@
+package com.oneread.crypto
+
+
+import com.google.crypto.tink.KeyTemplates
+import com.google.crypto.tink.aead.AeadConfig
+import com.google.crypto.tink.Aead
+import com.google.crypto.tink.KeysetHandle
+import com.google.crypto.tink.JsonKeysetReader
+import com.google.crypto.tink.JsonKeysetWriter
+import java.nio.file.Files
+import java.nio.file.Path
+
+
+object Crypto {
+init { AeadConfig.register() }
+
+
+fun loadOrCreateAead(keysetPath: Path): Aead {
+if (!Files.exists(keysetPath)) {
+Files.createDirectories(keysetPath.parent ?: Path.of("."))
+val handle = KeysetHandle.generateNew(KeyTemplates.get("AES256_GCM"))
+handle.write(JsonKeysetWriter.withPath(keysetPath), /* masterKey = */ null)
+}
+val handle = KeysetHandle.read(JsonKeysetReader.withPath(keysetPath), /* masterKey = */ null)
+return handle.getPrimitive(Aead::class.java)
+}
+}
