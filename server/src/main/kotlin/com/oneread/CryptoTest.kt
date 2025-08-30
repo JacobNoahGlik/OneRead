32548ee40c7b93275d9907b515bf5095cac17cca
Jacob Glik <70964953+JacobNoahGlik@users.noreply.github.com>
2025-08-30 11:56:35 -0400

    Update CryptoTest.kt

--- server/src/main/kotlin/com/oneread/CryptoTest.kt
+++ server/src/main/kotlin/com/oneread/CryptoTest.kt
@@ -8,13 +8,13 @@ import org.junit.jupiter.api.Test
 
 
 class CryptoTest {
-@Test
-fun roundtrip() {
-val tmp = Files.createTempFile("keyset", ".json")
-val aead = Crypto.loadOrCreateAead(tmp)
-val pt = "hello".toByteArray()
-val ct = aead.encrypt(pt, byteArrayOf())
-val got = aead.decrypt(ct, byteArrayOf())
-assertContentEquals(pt, got)
-}
+    @Test
+    fun roundtrip() {
+        val tmp = Files.createTempFile("keyset", ".json")
+        val aead = Crypto.loadOrCreateAead(tmp)
+        val pt = "hello".toByteArray()
+        val ct = aead.encrypt(pt, byteArrayOf())
+        val got = aead.decrypt(ct, byteArrayOf())
+        assertContentEquals(pt, got)
+    }
 }
