7fdddcccf85c2e1db9a1f8871f0e441bb134eca3
Jacob Glik <70964953+JacobNoahGlik@users.noreply.github.com>
2025-08-30 11:58:30 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-// Apply to subprojects in a convention block below
-id("com.diffplug.spotless") version "6.25.0"
-id("io.gitlab.arturbosch.detekt") version "1.23.6"
+    // Apply to subprojects in a convention block below
+    id("com.diffplug.spotless") version "6.25.0"
+    id("io.gitlab.arturbosch.detekt") version "1.23.6"
 }
 
 
@@ -9,47 +9,47 @@ val kotlinVersion = System.getenv("KOTLIN_VERSION") ?: "2.0.0"
 
 
 subprojects {
-repositories {
-mavenCentral()
-}
-
-
-// Apply Kotlin JVM to all modules
-plugins.apply("org.jetbrains.kotlin.jvm")
-
-
-extensions.configure(org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension::class.java) {
-jvmToolchain(21)
-}
-
-
-tasks.withType<Test>().configureEach {
-useJUnitPlatform()
-}
+    repositories {
+        mavenCentral()
+    }
+    
+    
+    // Apply Kotlin JVM to all modules
+    plugins.apply("org.jetbrains.kotlin.jvm")
+    
+    
+    extensions.configure(org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension::class.java) {
+        jvmToolchain(21)
+    }
+    
+    
+    tasks.withType<Test>().configureEach {
+        useJUnitPlatform()
+    }
 }
 
 
 spotless {
-kotlin {
-ktlint("1.2.1")
-target("**/*.kt")
-trimTrailingWhitespace()
-endWithNewline()
-}
-kotlinGradle {
-ktlint("1.2.1")
-target("**/*.kts")
-}
+    kotlin {
+        ktlint("1.2.1")
+        target("**/*.kt")
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+    kotlinGradle {
+        ktlint("1.2.1")
+        target("**/*.kts")
+    }
 }
 
 
 detekt {
-buildUponDefaultConfig = true
-config.setFrom(files(".detekt.yml"))
+    buildUponDefaultConfig = true
+    config.setFrom(files(".detekt.yml"))
 }
 
 
 // Make Kotlin plugin version available to subprojects
 gradle.beforeProject {
-project.extensions.extraProperties.set("kotlinVersion", kotlinVersion)
+    project.extensions.extraProperties.set("kotlinVersion", kotlinVersion)
 }
